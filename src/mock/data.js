5a7ae3699ecb613101e6f9d686364cd1ac665e4a
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-12-14 14:15:21 -0500

    updated email

--- src/mock/data.js
+++ src/mock/data.js
@@ -32,28 +32,6 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'Beergle.jpg',
-    title: 'Beergle',
-    info:
-      'My first project as a Software Engineer, built using Javascript, HTML5, CSS and styled with Flexbox.',
-    info2:
-      "I incorporated BrewDog Brewery's third-party API - PunkApi to build a search engine for BrewDog's Beers.",
-    url: 'https://sandyjacobson.github.io/Beergle/',
-    repo: 'https://github.com/SandyJacobson/Beergle', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'ManyThanks.jpg',
-    title: 'Many Thanks',
-    info:
-      'Built with React, Javascript and CSS. Deployed via Netlify and integrated with Airtable, I created a Full Stack Gratitude Journal with the ability to create, render, edit and delete entries.',
-    info2:
-      'In a world where everything we could ever want it only one click away, I thought it would be nice to have an app where people can take a step back and think about those in their lives who deserve a thanks!',
-    url: 'https://many-thanks.netlify.app',
-    repo: 'https://github.com/SandyJacobson/Many-Thanks', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
     img: 'Sportikus.jpg',
     title: 'Sportikus',
     info:
@@ -72,13 +50,35 @@ export const projectsData = [
     url: 'https://footballbros.netlify.app/',
     repo: 'https://github.com/SandyJacobson/footballbros', // if no repo, the button will not show up
   },
+  {
+    id: nanoid(),
+    img: 'ManyThanks.jpg',
+    title: 'Many Thanks',
+    info:
+      'Built with React, Javascript and CSS. Deployed via Netlify and integrated with Airtable, I created a Full Stack Gratitude Journal with the ability to create, render, edit and delete entries.',
+    info2:
+      'In a world where everything we could ever want it only one click away, I thought it would be nice to have an app where people can take a step back and think about those in their lives who deserve a thanks!',
+    url: 'https://many-thanks.netlify.app',
+    repo: 'https://github.com/SandyJacobson/Many-Thanks', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Beergle.jpg',
+    title: 'Beergle',
+    info:
+      'My first project as a Software Engineer, built using Javascript, HTML5, CSS and styled with Flexbox.',
+    info2:
+      "I incorporated BrewDog Brewery's third-party API - PunkApi to build a search engine for BrewDog's Beers.",
+    url: 'https://sandyjacobson.github.io/Beergle/',
+    repo: 'https://github.com/SandyJacobson/Beergle', // if no repo, the button will not show up
+  },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: 'sandy.david.jacobson@gmail.com',
+  email: 'sandy.d.jacobson@gmail.com',
 };
 
 // FOOTER DATA
